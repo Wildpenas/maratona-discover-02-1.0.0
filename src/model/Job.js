b7544623856584b8a02d37a725b9d82ce31f7b7a
Wild Penas <penaswild@gmail.com>
2021-06-09 21:21:56 +0200

    ## Refatorar Job 	* [X] Mover controller para JobController 	* [X] Criar pasta utils 	* [X] Mover services de job para utils 	* [X] Importar utils dentro de jobController 	* [X] Refatorar chamada de job services para JobUtils   * [X] Mover Job data para model Job 	* [X] Importar model Job dentro de Jobcontroller 	* [X] Refatorar chamadas de job.data para Job 	* [X] Criar update no model recebendo newData 	* [X] Criar delete no model

---
+++ src/model/Job.js
@@ -0,0 +1,38 @@
+let data = [{
+    id: 1,
+    name: "Pizzaria Guloso",
+     'daily-hours': 2,
+      'total-hours': 1,
+        create_at: Date.now(),     //atribuido data de criacao (hoje)
+       
+    },
+{
+    id: 2,
+    name: "OneTwo Project",
+     'daily-hours': 3,
+      'total-hours': 43,
+        create_at: Date.now(),    //atribuido data de criacao (hoje)
+       
+    }
+];
+
+module.exports = {
+
+    get(){
+
+        return data;
+
+    },
+
+    update(newJob){
+
+        data = newJob;
+    
+    },
+    delete(id){
+
+        data =  data.filter(job => Number(job.id) !== Number(id))
+
+    }
+
+}
